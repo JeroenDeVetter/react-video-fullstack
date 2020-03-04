ee4f1e5e5d183f3b2bc9e128fb0d04b3e160af89
Jeroen De Vetter <devetterjeroen@gmail.com>
2020-03-04 14:20:05 +0100

    Last commit
    I made a docs folder do deploy the website on github pages

---
+++ docs/precache-manifest.ac776fc865c1091e44b22e1d060e8d99.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ee1594444731924e51812eff695c8c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4164ec74ca2e08900ffb",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "4cdc2ff45f5438c50c7b",
+    "url": "/static/css/main.2dc33165.chunk.css"
+  },
+  {
+    "revision": "4164ec74ca2e08900ffb",
+    "url": "/static/js/2.cfe66e09.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/static/js/2.cfe66e09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cdc2ff45f5438c50c7b",
+    "url": "/static/js/main.ce831f8e.chunk.js"
+  },
+  {
+    "revision": "75729ffdf84eb6a68065",
+    "url": "/static/js/runtime-main.e4ecb8d0.js"
+  },
+  {
+    "revision": "862ad29193ecd3434ed92f662f67a8b4",
+    "url": "/static/media/noimg.862ad291.jpg"
+  }
+]);
